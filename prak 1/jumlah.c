8b3b2ac43a31dcea6ee889fa390e86976acdfdf1
justinjya <103380665+justinjya@users.noreply.github.com>
2022-10-26 23:20:16 +0700

    Update jumlah.c

--- prak 1/jumlah.c
+++ prak 1/jumlah.c
@@ -2,15 +2,8 @@
 
 int main()
 {
-    int x, digit, jumlah;
-    jumlah = 0;
-    scanf("%d", &x);
-    while (x > 0)
-    {
-        digit = x % 10;
-        jumlah += digit;
-        x = x / 10;
-    }
+    int x, y, jumlah;
+    jumlah = x + y;
     printf("%d\n", jumlah);
     return 0;
 }
